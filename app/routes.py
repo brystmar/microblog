2cc748662b719806663500d30665aa5584b6502d
Thomas <tp.berg@gmail.com>
2019-02-10 14:41:29 -0800

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 # when connecting from a web browser, show the Hello World page
+from flask import render_template
 from app import app
 
 # map the top-level URL to this function
@@ -8,16 +9,16 @@ from app import app
 
 # specify what to return
 def index():
-    text = '<p>'
-    text += '<h1><b>Breadsheet Central</b></h1>'
-    text += 'Determine the right schedule for FWSY recipes'
-    text += '</br></p>'
-    text += '<div label="recipelist"><p>'
-    text += '<ul>'
-    text += '<li>Country Sourdough: <i>Pain de Campagne</i></li>'
-    text += '<li>Overnight Poolish</li>'
-    text += '<li>Saturday White Loaf</li>'
-    text += '<li>Pizza Dough</li>'
-    text += '</ul>'
-    text += '</p></div>'
-    return text
+    user = {'username': 'tberg'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Seattle!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body':   'I love the TV show "I\'m Sorry"'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
