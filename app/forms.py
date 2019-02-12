b43447d4ab8cb599242354c94af39eae42b2cda9
Thomas <tp.berg@gmail.com>
2019-02-12 15:47:11 -0800

    Chapter 5: User Logins

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_confirm = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    # ensure username is unique
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username already in use.  Please select a different username.')
+
+    # ensure email address is unique
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email address already in use.  Please provide a different email address.')
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
 # when connecting from a web browser, show the Hello World page
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 @app.route('/')  # map the top-level URL to this function
 @app.route('/index')  # also map the /index URL to this same function
+@login_required  # require login to view this page
 # specify what to return
 def index():
-    user = {'username': 'tberg'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -20,15 +24,51 @@ def index():
             }
         ]
 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form_login = LoginForm()
-    if form_login.validate_on_submit():
-        # show a flashing message when login info is submitted
-        flash('Login requested for user {}, remember_me={}'.format(form_login.username.data, form_login.remember_me.data))
+    # if a user is already logged in, redirect them to the homepage
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
 
-    return render_template('login.html', title='Sign In', form=form_login)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()  # returns only the first result
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember=form.remember_me.data)
+        # grab the user's desired page from the querystring
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':  # no requested next page, or that URL is suspicious
+            next_page = url_for('index')
+        return redirect(next_page)  # redirect to this desired page after login
+
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # if a user is already logged in, they don't need to see the registration page
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful!  Please sign in.')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
