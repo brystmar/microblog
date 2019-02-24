c6e3871d81d9a9ebfcdd23eacc9bcd37d8bbb983
Thomas <tp.berg@gmail.com>
2019-02-23 20:20:28 -0800

    Chapter 11: Facelift (Bootstrap & CSS)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # microblog
 Flask tutorial from https://courses.miguelgrinberg.com/courses/flask-mega-tutorial/lectures/5183943
+
+Repo: https://github.com/miguelgrinberg/microblog/
--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 # error logging
 if not app.debug:  # don't send emails when in debug mode.  To use a virtual server instead: python -m smtpd -n -c DebuggingServer localhost:8025
